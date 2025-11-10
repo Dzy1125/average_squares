c312e325be2e3ebb64cf20d929d3e60e6e84d08e
Dzy1125 <2422492776@qq.com>
2025-11-10 00:27:32 +0000

    Fixed average_of_squares logic and updated doctests

--- squares.py
+++ squares.py
@@ -2,22 +2,22 @@
 
 
 def average_of_squares(list_of_numbers, list_of_weights=None):
-    """ Return the weighted average of a list of values.
+    """Return the weighted average of squares of numbers.
     
     By default, all values are equally weighted, but this can be changed
     by the list_of_weights argument.
-    
+
     Example:
     --------
     >>> average_of_squares([1, 2, 4])
-    7.0
+    21
     >>> average_of_squares([2, 4], [1, 0.5])
-    6.0
+    12.0
     >>> average_of_squares([1, 2, 4], [1, 0.5])
     Traceback (most recent call last):
     AssertionError: weights and numbers must have same length
-
     """
+    """Return the weighted average of squares of numbers."""
     if list_of_weights is not None:
         assert len(list_of_weights) == len(list_of_numbers), \
             "weights and numbers must have same length"
@@ -38,9 +38,9 @@ def convert_numbers(list_of_strings):
     Example:
     --------
     >>> convert_numbers(["4", " 8 ", "15 16", " 23    42 "])
-    [4, 8, 15, 16]
-
+    [4.0, 8.0, 15.0, 16.0, 23.0, 42.0]
     """
+
     all_numbers = []
     for s in list_of_strings:
         # Take each string in the list, split it into substrings separated by
